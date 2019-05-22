1729e4408e8391fa856cfbe1ee935e3beefc65da
Alpal94 <alasdair.penman@hotmail.com>
2019-05-22 23:14:29 +0000

    Deployment script for ease of deployment

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp dev/ausPost.php  production/
+cp dev/cron.sh  production/
+cp dev/octoPrintControl.php  production/
+cp dev/orderFetch.php  production/
+cp dev/prod-version.md  production/
